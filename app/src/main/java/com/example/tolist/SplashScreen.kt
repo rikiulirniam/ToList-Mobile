d6a2703179fe79b203c2ac3ef2eb43dc2b8eda0c
rikiulirniam <rikiulir@gmail.com>
2024-10-28 21:39:12 +0700

    init

---
+++ app/src/main/java/com/example/tolist/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.tolist
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }, 1500)
+
+    }
+}
